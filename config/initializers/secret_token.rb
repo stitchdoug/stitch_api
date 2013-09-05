dad4bb0b0fc20596af8f9523a072f1684e5febec
Doug Allen <doug@klablab.com>
2013-09-05 10:06:45 -0700

    Initial setup commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StitchApi::Application.config.secret_token = 'e65e0f25226b866aaffd14fd1f7caf9e52320906b1f030ada85d1ce233741821ef7860fdfe6b52bfdd6a33470328ba190450a2ff734f484f4f5cd7ee1c1b564c'
